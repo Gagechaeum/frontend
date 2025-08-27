26418e9478c1ef701ca705a71fe419f1aa10e458
saym010116 <saym0116@hanyang.ac.kr>
2025-08-27 10:30:04 +0900

    Chore : ESLint Flat Config 생성 및 Husky pre-commit v9 형식으로 정리

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+cd / c / Gageghaeum / frontend;
+
+cat > eslint.config.js << 'EOF';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  prettier,
+  {
+    files: ['**/*.{js,vue}'],
+    languageOptions: { ecmaVersion: 2021, sourceType: 'module' },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+];
+EOF;
