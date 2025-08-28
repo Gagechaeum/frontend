319e467ef71f8e224157ca1dee7eb61599c48832
김윤지 <kkamisister1207@gmail.com>
2025-08-28 12:16:27 +0900

    Chore : 앱 푸터 컴포넌트 import

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-neutral-50 text-neutral-900">
+  <div class="flex min-h-screen flex-col bg-neutral-50 text-neutral-900">
     <AppHeader v-if="$route.meta.showHeader !== false" />
     <main class="flex-1">
       <RouterView />
     </main>
+    <AppFooter />
   </div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
-import AppHeader from './components/AppHeader.vue'
+import { RouterView } from 'vue-router';
+import AppHeader from './components/common/AppHeader.vue';
+import AppFooter from './components/common/AppFooter.vue';
 </script>
