f4464697c765056f3985a7da344e58e81b38cbff
김윤지 <kkamisister1207@gmail.com>
2025-09-02 14:11:44 +0900

    Feat : Pinia 상태관리 및 Font Awesome 아이콘 시스템 도입

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // src/main.js
-import { createApp } from 'vue'
-import router from './router'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import './style.css';
+import App from './App.vue';
 
+// Font Awesome CSS import
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+app.mount('#app');
